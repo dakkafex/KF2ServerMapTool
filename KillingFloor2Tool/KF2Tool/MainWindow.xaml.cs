5082fe9c1e94a74988b6562b7e2b7a64af2af93f
bas ks <kazrhak@hotmail.com>
2016-02-20 05:44:47 +0100

    Working text and image link

--- KillingFloor2Tool/KF2Tool/MainWindow.xaml.cs
+++ KillingFloor2Tool/KF2Tool/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net;
+using System.Text.RegularExpressions;
 
 namespace KF2Tool
 {
@@ -20,9 +22,42 @@ namespace KF2Tool
     /// </summary>
     public partial class MainWindow : Window
     {
+
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void TestButton_Click(object sender, RoutedEventArgs e)
+        {
+            //This sets the information
+            string ScreenshotURL;
+            string NameMap;
+            string AuthorST;
+            string Author;
+            string Summary;
+
+            using (WebClient client = new WebClient())
+            {
+                string htmlCode = client.DownloadString("http://kf2.gamebanana.com/maps/188863");
+
+                NameMap = Regex.Match(htmlCode, @"<title>(.+?) ", RegexOptions.Singleline).Groups[1].Value;
+                ScreenshotURL = Regex.Match(htmlCode, @"<a class=\u0022Screenshot\u0022 href=\u0022(.+?).jpg\u0022>", RegexOptions.Singleline).Groups[1].Value + ".jpg";
+                AuthorST = Regex.Match(htmlCode, @"This is a Map submitted by(.+?)for", RegexOptions.Singleline).Groups[1].Value;
+                Author = Regex.Match(AuthorST, @"\u0022>(.+?)</a>", RegexOptions.Singleline).Groups[1].Value;
+                Summary = Regex.Match(htmlCode, @"<strong>Summary\u003A</strong></p><p>(.+?)</p>", RegexOptions.Singleline).Groups[1].Value;
+            }
+
+            MapName.Content = NameMap;
+            lblAuthor.Content = $"Map by: {Author}.";
+            summary.Text = Summary;
+            Screenshot.Source = new BitmapImage(new Uri(ScreenshotURL));
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
         }
     }
 }
---
+++ KillingFloor2Tool/test/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace test
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            using (WebClient client = new WebClient()) // WebClient class inherits IDisposable
+            {
+                //client.DownloadFile("http://stackoverflow.com/questions/599275/how-can-i-download-html-source-in-c-sharp", @"C:\users\jacky\documents\localfile.html");
+
+                // Or you can get the file content without saving it:
+                string htmlCode = client.DownloadString("http://kf2.gamebanana.com/maps/188863");
+                //...
+                string s = Regex.Match(AuthorST, @"<strong>Summary\u003A</strong></p><p>\u0022(.+?)</p>", RegexOptions.Singleline).Groups[1].Value;
+                Console.WriteLine(s);
+                Console.Read();
+            }
+        }
+    }
+}
